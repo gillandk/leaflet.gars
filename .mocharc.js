369b6ef890993f51ca67ca8fd05da10df269b5c3
Kevin Gilland <kgilland@caci.com>
2022-10-20 10:27:58 -0600

    chore: more repo setup

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    extensions: ['ts', 'js'],
+    ignore: ['node_modules/**'],
+    recursive: true,
+    require: ['ts-node/register'],
+    spec: ["test/**/*.spec.*"],
+    timeout: '30000'
+};
